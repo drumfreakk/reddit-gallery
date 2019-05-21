e62d6fab7262eddc34a4cd741abd31102e8b162b
Kieran H <kieran.houtgraaf@gmail.com>
2019-05-21 20:16:44 +0200

    Fading

--- redditgallery/static/js/hover.js
+++ redditgallery/static/js/hover.js
@@ -16,3 +16,42 @@ function loadHoverElements(){
 
 function hover_init(){
 }
+
+
+var myopacity = 0;
+
+function fadeIn() {
+   if (myopacity<1) {
+      myopacity += .075;
+     setTimeout(function(){MyFadeFunction()},100);
+   }
+   document.getElementById('navbar').style.opacity = myopacity;
+}
+
+function fadeOut() {
+   if (myopacity>0) {
+      myopacity -= .075;
+     setTimeout(function(){MyFadeFunction()},100);
+   }
+   document.getElementById('navbar').style.opacity = myopacity;
+}
+
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       fadeOut();
+   } else {
+      fadeIn();
+   }
+   lastScrollTop = st;
+});
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+		window.scrollBy(0, -100);
+		loadEls();
+    }
+};
+
